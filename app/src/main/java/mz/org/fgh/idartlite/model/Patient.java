b25e90f49866cd5a81123e7c493bbbaa48d9170e
josechambule <engchambule@gmail.com>
2020-09-18 10:02:40 +0200

    add new method and screen

--- app/src/main/java/mz/org/fgh/idartlite/model/Patient.java
+++ app/src/main/java/mz/org/fgh/idartlite/model/Patient.java
@@ -153,6 +153,10 @@ public class Patient extends BaseModel {
 		return data;
 	}
 
+	public String getFullName(){
+		return this.firstName + " " + this.lastName;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
--- app/src/main/java/mz/org/fgh/idartlite/view/SearchPatientActivity.java
+++ app/src/main/java/mz/org/fgh/idartlite/view/SearchPatientActivity.java
@@ -25,6 +25,7 @@ import mz.org.fgh.idartlite.base.BaseViewModel;
 import mz.org.fgh.idartlite.databinding.ActivitySearchPatientBinding;
 import mz.org.fgh.idartlite.generated.callback.OnClickListener;
 import mz.org.fgh.idartlite.model.Patient;
+import mz.org.fgh.idartlite.util.Utilities;
 import mz.org.fgh.idartlite.view.patient.PatientActivity;
 import mz.org.fgh.idartlite.viewmodel.PatientVM;
 
@@ -38,17 +39,17 @@ public class SearchPatientActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         searchPatientBinding = DataBindingUtil.setContentView(this, R.layout.activity_search_patient);
-        try {
+        /*try {
             displaySearchResult();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        searchPatientBinding.imageViewSearch.setOnClickListener(new View.OnClickListener(){
+        }*/
+        searchPatientBinding.buttonSearch.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(searchPatientBinding.edtSearchParam.getText() != null){
+                if(Utilities.stringHasValue(searchPatientBinding.edtSearchParam.getText().toString())){
                     searPatient();
-                    if(!patientList.isEmpty()){
+                    if(Utilities.listHasElements(patientList)){
                         try {
                             displaySearchResult();
                         } catch (SQLException e) {
@@ -65,8 +66,8 @@ public class SearchPatientActivity extends BaseActivity {
 
     public void searPatient(){
         try {
-            //patientList = getRelatedViewModel().searchPatient(searchPatientBinding.edtSearchParam.getText().toString());
-            patientList = getRelatedViewModel().getAllPatient();
+            patientList = getRelatedViewModel().searchPatient(searchPatientBinding.edtSearchParam.getText().toString());
+            //patientList = getRelatedViewModel().getAllPatient();
             System.out.println(patientList.get(0).getFirstName());
         } catch (SQLException e) {
             e.printStackTrace();
@@ -74,7 +75,8 @@ public class SearchPatientActivity extends BaseActivity {
     }
 
     public void displaySearchResult() throws SQLException {
-        patientList = getRelatedViewModel().getAllPatient();
+        //patientList = getRelatedViewModel().getAllPatient();
+        //searPatient();
         recyclerPatient = searchPatientBinding.reyclerPatient;
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerPatient.setLayoutManager(layoutManager);
