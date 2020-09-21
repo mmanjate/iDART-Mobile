c6ad436e2d66e9c32cdd1c652b4f7d6821dbcef7
josechambule <engchambule@gmail.com>
2020-09-21 21:50:43 +0200

    add new method and screen

---
+++ app/src/main/java/mz/org/fgh/idartlite/adapter/StockTabAdapter.java
@@ -0,0 +1,42 @@
+package mz.org.fgh.idartlite.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockTabAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<Fragment>();
+    private final List<String> mFragmentTitleList = new ArrayList<String>();
+
+    public StockTabAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
