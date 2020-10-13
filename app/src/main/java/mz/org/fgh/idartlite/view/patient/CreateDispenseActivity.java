ac99b98752e2ac6f4ef39698f4d5f11136107312
nelson <nelson.homo@fgh.org.mz>
2020-10-13 14:58:36 +0200

    fluxo adicao de aviamento vindo da prescricao

--- app/src/main/java/mz/org/fgh/idartlite/view/patient/CreateDispenseActivity.java
+++ app/src/main/java/mz/org/fgh/idartlite/view/patient/CreateDispenseActivity.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
