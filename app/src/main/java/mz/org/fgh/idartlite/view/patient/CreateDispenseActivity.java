065c4929a90d429cf2a3b3d2eece42e782c705a3
josechambule <engchambule@gmail.com>
2020-10-14 13:09:26 +0200

    toolbar

--- app/src/main/java/mz/org/fgh/idartlite/view/patient/CreateDispenseActivity.java
+++ app/src/main/java/mz/org/fgh/idartlite/view/patient/CreateDispenseActivity.java
@@ -120,58 +120,61 @@ public class CreateDispenseActivity extends BaseActivity implements DialogListen
         if (getApplicationStep().isApplicationStepDisplay())
             getRelatedViewModel().setViewListRemoveButton(false);
 
-        activityCreateDispenseBinding.dispenseDate.setOnClickListener(new View.OnClickListener() {
+        activityCreateDispenseBinding.dispenseDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View view) {
-                int mYear, mMonth, mDay;
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    int mYear, mMonth, mDay;
 
-                final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
+                    final Calendar c = Calendar.getInstance();
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
 
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(CreateDispenseActivity.this, new DatePickerDialog.OnDateSetListener() {
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateDispenseActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @RequiresApi(api = Build.VERSION_CODES.O)
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-                    @RequiresApi(api = Build.VERSION_CODES.O)
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            activityCreateDispenseBinding.dispenseDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                        activityCreateDispenseBinding.dispenseDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            int duracaoDoAviamento = ((ValorSimples) activityCreateDispenseBinding.spnDuration.getSelectedItem()).getId();
 
-                        int duracaoDoAviamento = ((ValorSimples) activityCreateDispenseBinding.spnDuration.getSelectedItem()).getId();
+                            String nextPickUpDate = calculateNextPickUpDate(duracaoDoAviamento);
+                            if (duracaoDoAviamento != 0)
+                                activityCreateDispenseBinding.nextPickupDate.setText(nextPickUpDate);
 
-                        String nextPickUpDate = calculateNextPickUpDate(duracaoDoAviamento);
-                        if (duracaoDoAviamento != 0)
-                            activityCreateDispenseBinding.nextPickupDate.setText(nextPickUpDate);
-
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                        }
+                    }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                }
             }
         });
 
-        activityCreateDispenseBinding.nextPickupDate.setOnClickListener(new View.OnClickListener() {
+        activityCreateDispenseBinding.nextPickupDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View view) {
-                int mYear, mMonth, mDay;
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    int mYear, mMonth, mDay;
 
-                final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
+                    final Calendar c = Calendar.getInstance();
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateDispenseActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(CreateDispenseActivity.this, new DatePickerDialog.OnDateSetListener() {
 
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-                        activityCreateDispenseBinding.nextPickupDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            activityCreateDispenseBinding.nextPickupDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                    }
-                }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+                        }
+                    }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                }
             }
         });
 
