b5a6b47df312799b08ea33a07b55eb9434efbddc
voloide <voloidet@gmail.com>
2022-01-25 12:17:58 +0200

    Dispensa de paciente faltoso

--- app/src/main/assets/from_11_to_12.sql
+++ app/src/main/assets/from_11_to_12.sql
@@ -1,5 +1,6 @@
 ALTER TABLE clinicSector ADD COLUMN clinic_sector_type_id INTEGER;
-ALTER TABLE dispense_drug ADD COLUMN drug_id INTEGER
+ALTER TABLE dispense_drug ADD COLUMN drug_id INTEGER;
+UPDATE Dispense_drug SET drug_id = (SELECT drug_id FROM Stock WHERE id = Dispense_drug.stock_id) WHERE drug_id is null;
 INSERT INTO patient_attribute (attribute, value, patient_id) SELECT 'PATIENT_DISPENSATION_STATUS', 'Activo', id FROM patient WHERE NOT EXISTS (SELECT * FROM patient_attribute WHERE patient_id = patient.id and attribute = 'PATIENT_DISPENSATION_STATUS');
 INSERT INTO clinic_sector_type (description, code) values ('Paragem Única', 'PARAGEM_UNICA');
 INSERT INTO clinic_sector_type (description, code) values ('Provedor', 'PROVEDOR');
